3a939d11beb930e04a0c41b14fee0785494b927d
Nic Roland <nicroland9@gmail.com>
2016-02-01 08:33:19 +0000

    Added code to actually download podcast episodes.

--- backend/hathor.go
+++ backend/hathor.go
@@ -4,15 +4,46 @@ import (
 	"fmt"
 	rss "github.com/jteeuwen/go-pkg-rss"
 	"github.com/nicr9/hathor/backend/hathor"
+	"io"
+	"net/http"
 	"os"
 	"time"
 )
 
 func download(episode hathor.Episode) {
-	fmt.Printf("%s - Downloading '%s'\n", episode.Key, episode.Title)
 	err := os.MkdirAll(episode.DirPath, 0755)
 	if err != nil {
 		fmt.Printf("[e] Couldn't make dir: %s\n -> %s", episode.DirPath, err)
+		return
+	}
+
+	// Only download if it's not already cached
+	if _, err := os.Stat(episode.FilePath); os.IsNotExist(err) {
+		fmt.Printf("%s - Downloading '%s'\n", episode.Key, episode.Title)
+
+		// Create the file
+		out, err := os.Create(episode.FilePath)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		defer out.Close()
+
+		// Get the data
+		resp, err := http.Get(episode.Url)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		defer resp.Body.Close()
+
+		// Writer the body to file
+		_, err = io.Copy(out, resp.Body)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		fmt.Printf("%s - Finished '%s'\n", episode.Key, episode.Title)
 	}
 }
 
